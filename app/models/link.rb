2582dc8a94d07da67c32a8f5735b13ba79f43423
Guest User <student@epicodus.com>
2015-06-30 15:27:50 -0700

    Added URL validation.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,14 @@
 class Link < ActiveRecord::Base
+  before_save :strip_url
   validates :name, :presence => true
   validates :url, :presence => true
+
+  private
+  def strip_url
+    self.url.sub!(/https\:\/\/www./, '') if self.url.include? "https://www."
+    self.url.sub!(/http\:\/\/www./, '')  if self.url.include? "http://www."
+    self.url.sub!(/www./, '')            if self.url.include? "www."
+    return url
+  end
+
 end
